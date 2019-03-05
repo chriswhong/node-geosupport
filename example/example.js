5c682f48a0caf44f6b9d9eae91848fed94ad6218
Chris Whong <chris@whong.org>
2019-03-04 21:30:16 -0500

    WIP wa1 builder working

--- example/example.js
+++ example/example.js
@@ -1,11 +1,13 @@
 const geosupport = require('../index.js');
 
 const params = {
-  houseNumber: '86-54',
-  streetName: 'WINCHESTER BOULEVARD',
-  zipCode: '11427',
-  boroCode: 4,
+  'House Number - Display Format': '86-54',
+  'Street Name-1': 'WINCHESTER BOULEVARD',
+  'ZIP Code Input': '11427',
+  'Borough Code': '4',
 };
 
-const result = geosupport['1B'](params);
-console.log(result);
+const result = geosupport['1B'](params)
+  .then((response) => {
+    console.log(response);
+  });
--- src/node-geosupport.js
+++ src/node-geosupport.js
@@ -1,5 +1,6 @@
 const ffi = require('ffi');
 const parseBuffer = require('./parseBuffer');
+const csv = require('csvtojson');
 
 require('dotenv').config();
 
@@ -23,8 +24,8 @@ const callGeolib = (wa1, wa2) => {
   const wa1Buffer = new Buffer(1200);
   const wa2Buffer = new Buffer(4300);
 
-  wa1Buffer.write(wa1, 'utf8');
-  wa2Buffer.write(wa2, 'utf8');
+  wa1Buffer.write(wa1);
+  wa2Buffer.write(wa2);
 
   lib.geo(wa1Buffer, wa2Buffer);
 
@@ -34,16 +35,48 @@ const callGeolib = (wa1, wa2) => {
   };
 };
 
-const call = (params) => {
-  const { houseNumber, boroCode, streetName, zipCode } = params;
-  const wa1 = `1B${rightpad(houseNumber, 16)}${rightpad('', 38)}${boroCode}${rightpad('', 10)}${rightpad(streetName, 32)}C${rightpad(zipCode, 5)}`;
+const createWa1 = async (params) => {
+  params['Work Area Format Indicator'] = 'C';
+
+  const layouts = await csv().fromFile('src/function_info/work_area_layouts/input/WA1.csv');
+
+  const buffer = new Buffer(1200);
+  buffer.fill(' ');
+
+  Object.keys(params).forEach((key) => {
+    const layout = layouts.find(d => d.name === key);
+    buffer.write(params[key], parseInt(layout.from, 10) - 1, parseInt(layout.size, 10));
+  });
+  console.log('O HI', buffer.toString().substring(0, 2))
+
+  return buffer.toString();
+}
+
+const createWa2 = (params) => {
+ return ''
+}
+
+const formatInput = async (params) => {
+  const wa1 = await createWa1(params);
   const wa2 = '';
 
+  return { wa1, wa2 };
+}
+
+const call = async (params) => {
+  params['Geosupport Function Code'] = '1A';
+  // const { houseNumber, boroCode, streetName, zipCode } = params;
+  // const wa1 = `1B${rightpad(houseNumber, 16)}${rightpad('', 38)}${boroCode}${rightpad('', 10)}${rightpad(streetName, 32)}C${rightpad(zipCode, 5)}`;
+  // const wa2 = '';
+
+  const { wa1, wa2 } = await formatInput(params);
+  console.log('here', wa1)
+
   const { wa1Response, wa2Response } = callGeolib(wa1, wa2);
+  console.log('there', wa1Response)
 
   return parseBuffer(wa1Response, wa2Response);
 };
-
 
 module.exports = {
   '1B': call,
